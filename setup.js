bd03aaa3b43e987f1a44316db1eec267ccee1085
Cob <adipo12345678@gmail.com>
2022-04-12 17:39:38 -0400

    Add files via upload

---
+++ setup.js
@@ -0,0 +1,172 @@
+﻿var soundFiles = [], sounds = [], editing = true;
+async function saveSounds(saveData = JSON.stringify(sounds)) {
+    saveFile = await soundDirectory.getFileHandle("sounds.json", { create: true })
+    fileWrite = await saveFile.createWritable()
+    await fileWrite.write(saveData)
+    await fileWrite.close()
+    editing = false;
+    loadSounds();
+}
+function saveButtonClick() {
+    document.getElementById('saveStatus').innerHTML = 'Saving...';
+    document.getElementById("saveAllButton").disabled = true;
+	document.body.classList.add("saving")
+    saveSounds().then(() => {
+        document.getElementById('saveStatus').innerHTML = 'Done!';
+        document.getElementById("saveAllButton").disabled = false;
+		document.body.classList.remove("saving")
+    })
+}
+function editSounds() {
+    editing = true;
+    loadSounds(["edit"]);
+}
+function editSingleSound(sound) {
+    sound.elem.replaceWith(sound.createTableRow(document.getElementById("soundTBody"), true))
+    addActions(sound.elem, ["edit"]);
+}
+async function loadJSON() {
+    var savedSounds = await saveFile.text().then(e => JSON.parse(e, (key, val) => !isNaN(key) && !Array.isArray(val) ? Object.assign(new Sound(), val) : val))
+    //Parses JSON data while replacing replacing object literals with Sounds
+    savedSounds = savedSounds.filter((e, i) => sounds.map(e=>e.file.name).includes(e.file))
+    for (var i of savedSounds) {
+        i.file = await (await soundDirectory.getFileHandle(i.file)).getFile();
+    }
+    sounds.map((e, i) => {
+        if (!savedSounds.map(a => a.file.name).includes(e.file.name)) {
+            savedSounds.push(e)
+        }
+    })
+    sounds = savedSounds
+}
+function loadSounds(blockActions) {
+    var table = document.getElementById("soundTBody")
+    for (var i in sounds) {
+        var row = sounds[i].createTableRow(table, editing)
+        if (table.children[i]) {
+            table.children[i].replaceWith(row)
+        } else {
+            table.appendChild(row)
+        }
+        addActions(row, blockActions)
+    }
+}
+function addActions(elem, blockActions = []) {
+    var cell = document.createElement("td");
+    cell.prop = "actions";
+    cell.setAttribute("prop", "actions");
+    if (elem.querySelector("[prop=actions]")) {
+        elem.replaceChild(cell, elem.querySelector("[prop=actions]"))
+    } else {
+        elem.appendChild(cell)
+    }
+    function addEditButton() {
+        var editButton = cell.appendChild(document.createElement("button"))
+        editButton.classList.add("editButton")
+        editButton.title = "Edit"
+        editButton.innerText = "🖉"
+        editButton.onclick = e => editSingleSound(e.target.parentElement.parentElement.sound)
+    }
+    function addSaveButton() {
+        var saveButton = cell.appendChild(document.createElement("button"))
+        saveButton.classList.add("saveButton")
+        saveButton.title = "Save"
+        saveButton.innerText = "🖫"
+        saveButton.onclick = () => saveButtonClick()
+    }
+    function addVolumeMirrorButton() {
+        var volumeMirrorButton = cell.appendChild(document.createElement("button"))
+        volumeMirrorButton.classList.add("volumeMirrorButton")
+        volumeMirrorButton.title = "Save current volume"
+        volumeMirrorButton.innerText = "🕪🖫"
+        volumeMirrorButton.onclick = e => {e.target.parentElement.parentElement.sound.setProp("volume", e.target.parentElement.parentElement.sound.audioElem.volume * 100); if(!editing){saveButtonClick()}}
+    }
+    function addSoundVolumeMirrorButton() {
+        var soundVolumeMirrorButton = cell.appendChild(document.createElement("button"))
+        soundVolumeMirrorButton.classList.add("soundVolumeMirrorButton")
+        soundVolumeMirrorButton.title = "Set current volume to saved volume"
+        soundVolumeMirrorButton.innerText = "🖫 → 🕪"
+        soundVolumeMirrorButton.onclick = e => e.target.parentElement.parentElement.sound.audioElem.volume = e.target.parentElement.parentElement.sound.volume / 100;
+    }
+    function addStartMirrorButton() {
+        var startMirrorButton = cell.appendChild(document.createElement("button"))
+        startMirrorButton.classList.add("startMirrorButton")
+        startMirrorButton.title = "Save current time"
+        startMirrorButton.innerText = "◷🖫"
+        startMirrorButton.onclick = e => {e.target.parentElement.parentElement.sound.setProp("start", e.target.parentElement.parentElement.sound.audioElem.currentTime); if(!editing){saveButtonClick()}}
+    }
+    function addTimeMirrorButton() {
+        var timeMirrorButton = cell.appendChild(document.createElement("button"))
+        timeMirrorButton.classList.add("timeMirrorButton")
+        timeMirrorButton.title = "Set audio time to saved time"
+        timeMirrorButton.innerText = "🖫 → ◷"
+        timeMirrorButton.onclick = e => e.target.parentElement.parentElement.sound.audioElem.currentTime = e.target.parentElement.parentElement.sound.start;
+    }
+	function addFadeButton() {
+        var fadeInput = cell.appendChild(document.createElement("input"))
+        fadeInput.classList.add("fadeInput");
+        fadeInput.type = "number";
+        fadeInput.value = 5;
+        fadeInput.title = "Time(in seconds) to fade out";
+		var fadeButton = cell.appendChild(document.createElement("button"))
+		fadeButton.classList.add("fadeButton");
+		fadeButton.title = "Fade out sound across given time";
+        fadeButton.innerText = "🕪→🕨"
+		fadeButton.onclick = e => e.target.parentElement.parentElement.sound.timeFade(fadeInput.value);
+	}
+    if (!blockActions.includes("edit")) {
+        addEditButton()
+    }
+    if (!blockActions.includes("save")) {
+        addSaveButton()
+    }
+    if (!blockActions.includes("volumeMirror")) {
+        addVolumeMirrorButton()
+    }
+    if (!blockActions.includes("soundVolumeMirror")) {
+        addSoundVolumeMirrorButton()
+    }
+    if (!blockActions.includes("startMirror")) {
+        addStartMirrorButton()
+    }
+    if (!blockActions.includes("timeMirror")) {
+        addTimeMirrorButton()
+    }
+    if (!blockActions.includes("fade")) {
+        addFadeButton()
+    }
+}
+async function openFiles() {
+    document.getElementById("openFilesButton").classList.remove("unopened");
+    soundDirectory = await window.showDirectoryPicker()
+    if (await soundDirectory.queryPermission({ mode: "readwrite" }) != "granted") {
+        await soundDirectory.requestPermission({ mode: "readwrite" })
+    }
+    for await (i of soundDirectory.values()) {
+        await i.getFile().then(e => e.type.startsWith("audio") ? sounds.push(new Sound(e, e.name)) : e.type == "application/json" ? saveFile = e: e)
+        //if file is not JSON, add it to sound file directory, otherwise store it in config var
+    }
+    if (typeof saveFile !== "undefined") {
+        await loadJSON()
+        editing = false;
+    } else {
+        await saveSounds()
+        editing = true;
+    }
+    loadSounds()
+}
+async function uploadFile() {
+    document.getElementById("uploadFileButton").disabled = true;
+    newFiles = await window.showOpenFilePicker({multiple: true});
+    for (i in newFiles){
+        newFiles[i] = await newFiles[i].getFile()
+        var newFileHandle = await soundDirectory.getFileHandle(newFiles[i].name, {create:true})
+        var newFileWritable = await newFileHandle.createWritable()
+        await newFileWritable.write(newFiles[i])
+		await newFileWritable.close()
+        newFiles[i] = new Sound(newFiles[i])
+    }
+    sounds = sounds.concat(newFiles);
+    loadSounds()
+    document.getElementById("uploadFileButton").disabled = false;
+}
---
+++ sound.js
@@ -0,0 +1,138 @@
+﻿class Sound {
+	constructor(file, name = file ? file.name : "", page = 0, ref = "", volume = 100, start = 0, notes = "") {
+		this.page = page
+		this.ref = ref
+		this.name = name
+		this.file = file
+		this.volume = volume
+		this.start = start
+		this.notes = notes
+	}
+	setProp(prop, val) {
+		this[prop] = val
+		if (this.elem) {
+			if (this.input) {
+				this.elem.querySelector(`[prop=${prop}] input`).value = this[prop];
+			} else {
+				this.elem.querySelector(`[prop=${prop}]`).innerText = this[prop];
+			}
+			if (prop == "volume") {
+				this.audioElem.volume = this[prop] / 100
+			}
+			if (prop == "start" && this.audioElem.paused) {
+				this.audioElem.currentTime = this[prop]
+			}
+		}
+	}
+	createTableRow(table, input = false) {
+		this.elem = document.createElement("tr")
+		this.elem.sound = this
+		this.elem.classList.add("soundRow")
+		this.input = input;
+		for (var i of table.previousElementSibling.children[0].children) {
+			var cell = this.elem.appendChild(document.createElement("td"))
+			cell.prop = i.getAttribute("prop")
+			cell.setAttribute("prop", cell.prop)
+			if (cell.prop == "actions") {
+				continue
+			} else if (cell.prop == "currentVol") {
+				cell.innerText = this.audioElem.volume * 100
+				continue
+			} else if (cell.prop == "currentTime") {
+				cell.innerText = this.audioElem.currentTime
+				continue
+			} else if (cell.prop == "file") {
+				if (this.audioElem) {
+					cell.appendChild(this.audioElem)
+					this.audioElem.load();
+				} else {
+					this.audioElem = cell.appendChild(document.createElement("audio"))
+					this.audioElem.src = URL.createObjectURL(this.file)
+					this.audioElem.controls = true
+					this.audioElem.preload = "metadata"
+					this.audioElem.load();
+					this.audioElem.onloadedmetadata = () => {
+						this.elem.querySelector("[prop =currentVol").innerText = this.audioElem.volume * 100;
+						this.elem.querySelector("[prop=currentTime]").innerText = this.audioElem.currentTime;
+						if (this.input) {
+							this.elem.querySelector("[prop =start").firstElementChild.max = this.audioElem.duration;
+							this.elem.querySelector("[prop =volume").firstElementChild.value = this.audioElem.volume * 100;
+						};
+					}
+					this.audioElem.onvolumechange = () => {
+						this.elem.querySelector("[prop=currentVol").innerText = this.audioElem.volume * 100;
+					}
+					this.audioElem.ontimeupdate = () => {
+						this.elem.querySelector("[prop=currentTime]").innerText = this.audioElem.currentTime;
+					}
+				}
+				this.audioElem.currentTime = this.start || 0;
+				this.audioElem.volume = this.volume / 100;
+			} else if (this.input) {
+				var cellInput = cell.appendChild(document.createElement("input"))
+				cellInput.value = this[cell.prop]
+				if (typeof this[cell.prop] == "number") {
+					cellInput.type = "number"
+					if (cell.prop == "volume" || cell.prop == "start") {
+						cellInput.min = 0;
+						cellInput.max = 100;
+						if (cell.prop == "start" && this.audioElem.readyState >= 1) {
+							cellInput.max = this.audioElem.duration;
+						}
+					}
+				}
+				cellInput.oninput = e => {
+					if (!e.target.validity.valid || !e.target.parentElement.prop) {
+						return
+					}
+					var prop = e.target.parentElement.prop
+					this[prop] = e.target.type == "number" ? e.target.valueAsNumber : e.target.value;
+					if (prop == "volume") {
+						this.audioElem.volume = this[prop] / 100;
+					} else if (prop == "start" && this.audioElem.paused) {
+						this.audioElem.currentTime = this[prop];
+					}
+				}
+			} else {
+				cell.innerText = this[cell.prop]
+			}
+		}
+		this.audioElem.load();
+		return this.elem
+	}
+	toJSON() {
+		var reduced = {}
+		for (i in this) {
+			if (i == "file") {
+				reduced[i] = this[i].name
+			} else if (i == "elem" || i == "audioElem") {
+				continue
+			} else {
+				reduced[i] = this[i]
+			}
+		}
+		return reduced
+	}
+	async factorFade(factor = .95) {
+		if (this.audioElem.readyState != 4) {
+			return;
+		}
+		this.fadeFactor = factor;
+		this.fadeStarted = Date.now();
+		return new Promise(resolve => {
+			this.fade = setInterval(() => {
+				if (this.audioElem.volume <= .01) {
+					this.audioElem.volume = 0;
+					this.audioElem.pause()
+					clearInterval(this.fade);
+					resolve(Date.now() - this.fadeStarted);
+				} else {
+					this.audioElem.volume *= factor;
+				}
+			}, 50)
+		})
+	}
+	async timeFade(time=4.5) {
+		return await this.factorFade((.99 / (100 * this.audioElem.volume)) ** (50 / (1000 * time - 50)))
+	}
+}
